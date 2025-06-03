5b3db27fcdfa227f6f2f945c4533e13720b503e6
arjunkorath <arjunkorath@rediffmail.com>
2025-06-03 09:58:24 +0530

    Fix TypeScript configuration

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,9 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin'
+      'react-native-reanimated/plugin',
+      '@babel/plugin-proposal-export-namespace-from',
+      'expo-router/babel'
     ],
   };
 };
